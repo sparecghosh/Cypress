7aeeaeeabd26db0c4005e3516c9ce451ac93ca8a
sparecghosh <sparecghosh@gmail.com>
2025-10-06 12:00:25 +0100

    fixed the dynamic data assertion issue

--- cypress/e2e/AddUser.cy.js
+++ cypress/e2e/AddUser.cy.js
@@ -1,20 +1,21 @@
 /// <reference types="cypress" />
 import  { HomePage } from '../Pages/HomePage/HomePageMethods';
 
+
 describe('Add User Test', () => {
     it('Add user using Page Object Model', () => {
         cy.visit('https://parabank.parasoft.com/parabank');
 
         const reg = HomePage.clickLoginButton();
         reg.fillRegistrationForm();
-        
+        var abc = reg.getRegistrationData();
+
         // Debug: Let's see what's actually on the page
         cy.get('#rightPanel').then($el => {
             cy.log('Right panel content:', $el.text());
         });
-        
-        // Check for successful registration - could be different text
-        cy.get('#rightPanel').should('be.visible')
-            .and('contain.text', 'Welcome');
+
+                cy.get('#rightPanel').should('be.visible')
+            .and('contain.text', `Welcome ${reg.getRegistrationData().username}`);
     });
 });
--- cypress/support/DataGenerator.js
+++ cypress/support/DataGenerator.js
@@ -12,7 +12,7 @@ export const DataGenerator = {
       zipCode: faker.location.zipCode(),
       phoneNumber: faker.phone.number('###-###-####'),
       ssn: faker.string.numeric(9),
-      username: 'user_' + faker.string.uuid(),
+      username: faker.internet.username(),
       password: faker.internet.password({ length: 12, memorable: false, pattern: /[A-Za-z0-9]/ }),
       ...overrides,
     };
