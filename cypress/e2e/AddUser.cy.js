cf27bf6e78f73e8d48eb0cb457addc90587f5ec7
sparecghosh <sparecghosh@gmail.com>
2025-10-06 12:01:54 +0100

    removed unwanted code

--- cypress/e2e/AddUser.cy.js
+++ cypress/e2e/AddUser.cy.js
@@ -8,14 +8,8 @@ describe('Add User Test', () => {
 
         const reg = HomePage.clickLoginButton();
         reg.fillRegistrationForm();
-        var abc = reg.getRegistrationData();
 
-        // Debug: Let's see what's actually on the page
-        cy.get('#rightPanel').then($el => {
-            cy.log('Right panel content:', $el.text());
-        });
-
-                cy.get('#rightPanel').should('be.visible')
-            .and('contain.text', `Welcome ${reg.getRegistrationData().username}`);
+        cy.get('#rightPanel').should('be.visible')
+           .and('contain.text', `Welcome ${reg.getRegistrationData().username}`);
     });
 });
